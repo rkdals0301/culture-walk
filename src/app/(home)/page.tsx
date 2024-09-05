0f5b57f60e7f36e15cc46d06c65a820807e51639
rkdals0301 <rkdals0301@naver.com>
2024-09-06 02:31:26 +0900

    update easter egg

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,7 +3,9 @@ import styles from '@/app/(home)/page.module.scss';
 export default function Home() {
   return (
     <div className={styles.main}>
-      <h1>문화산책</h1>
+      <h1>
+        <a href='https://comic.naver.com/challenge/detail?titleId=831482&no=1'>사이버렉카</a>
+      </h1>
     </div>
   );
 }
