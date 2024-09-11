db2d9024f692fc944ea3f61976852ee62ae5ee0b
rkdals0301 <rkdals0301@naver.com>
2024-09-12 01:57:08 +0900

    add channelTalk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -107,7 +107,6 @@ const RootLayout = ({
   return (
     <html lang='ko'>
       <head>
-        {/* 네이버 사이트 인증 메타 태그 추가 */}
         <Script
           id='google-analytics'
           strategy='beforeInteractive'
@@ -121,6 +120,20 @@ const RootLayout = ({
               gtag('config', 'G-Y4XKZDK818');
             `}
         </Script>
+        <Script
+          id='channel-talk-script'
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `
+  (function(){var w=window;if(w.ChannelIO){return w.console.error("ChannelIO script included twice.");}var ch=function(){ch.c(arguments);};ch.q=[];ch.c=function(args){ch.q.push(args);};w.ChannelIO=ch;function l(){if(w.ChannelIOInitialized){return;}w.ChannelIOInitialized=true;var s=document.createElement("script");s.type="text/javascript";s.async=true;s.src="https://cdn.channel.io/plugin/ch-plugin-web.js";var x=document.getElementsByTagName("script")[0];if(x.parentNode){x.parentNode.insertBefore(s,x);}}if(document.readyState==="complete"){l();}else{w.addEventListener("DOMContentLoaded",l);w.addEventListener("load",l);}})();
+
+  ChannelIO('boot', {
+    "pluginKey": "4ef79cde-b936-48a0-a501-2ccda61855e4",
+    "zIndex": 1,
+  });
+            `,
+          }}
+        />
       </head>
       <body className={notoSansKr.className}>
         <ThemeProvider>
