518d3b8baa5b9d4dd58bfca301a147a6b74992ae
rkdals0301 <rkdals0301@naver.com>
2024-09-19 00:53:22 +0900

    Update PrismaClient

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  log: ['query', 'info', 'warn', 'error'], // 로그 레벨
+  errorFormat: 'pretty', // 에러 형식
+});
 export default prisma;
