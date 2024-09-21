7db811a17c4639a310449638cda50e3156aeeb52
rkdals0301 <rkdals0301@naver.com>
2024-09-21 23:09:49 +0900

    Refactoring

--- prisma/migrations/20240921114051_add_unique_key/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[title,homepageAddress]` on the table `Culture` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Culture_title_homepageAddress_key" ON "Culture"("title", "homepageAddress");
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,5 @@
 import { PrismaClient } from '@prisma/client';
 
-const isProduction = process.env.NODE_ENV === 'production';
-
-const prisma = new PrismaClient({
-  log: isProduction ? ['error', 'warn'] : ['query', 'info', 'warn', 'error'],
-});
+const prisma = new PrismaClient();
 
 export default prisma;
