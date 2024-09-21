12c3815c777b142378f4a35bcad8700021dc093f
rkdals0301 <rkdals0301@naver.com>
2024-09-21 21:27:13 +0900

    Refacotring

--- src/app/api/initialize/route.ts
+++ src/app/api/initialize/route.ts
@@ -6,8 +6,8 @@ import { RawCulture, Culture } from '@/types/culture';
 const BASE_URL = process.env.SEOUL_API_CULTURAL_URL;
 const INITIAL_START_INDEX = 1;
 const PAGE_SIZE = 1000;
-const BATCH_SIZE = 50; // 배치 크기 조정
-const MAX_CONCURRENT_BATCHES = 5; // 동시에 처리할 배치 수 제한
+const BATCH_SIZE = 1000; // 배치 크기 조정
+const MAX_CONCURRENT_BATCHES = 50; // 동시에 처리할 배치 수 제한
 
 const fetchCultures = async (): Promise<RawCulture[]> => {
   const allCultures: RawCulture[] = [];
